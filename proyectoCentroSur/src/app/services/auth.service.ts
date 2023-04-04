d858ee9296890ae9a97c7862d21b50af69c71a3c
Esteban <estebannovilloq@gmail.com>
2023-04-04 15:45:20 -0500

    Se actualiza el esqueleto del auth service

--- proyectoCentroSur/src/app/services/auth.service.ts
+++ proyectoCentroSur/src/app/services/auth.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.development';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { 
+
+  }
+
+
+  login(username: string, password: string){
+    let url = environment.apiURL+"";
+
+    //Falta armar el paquete para consumir el servicio web
+
+
+    this.http.post<any>(url,"").subscribe(
+      data => {
+        //Metodos para inicio de sesion positiva o negativa
+        if(data){
+          
+          //Se debe permitir la navegacion
+          this.router.navigate(['']);
+        } else{
+
+          
+        }
+
+      }
+    );
+  }
 }
