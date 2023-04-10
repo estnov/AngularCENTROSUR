812c647d4ded4613ef7d1f1a1dc3a67c008fd99b
Esteban <estebannovilloq@gmail.com>
2023-04-10 09:08:59 -0500

    Actualizacion Servicios

--- proyectoCentroSur/src/app/Components/login/login.component.ts
+++ proyectoCentroSur/src/app/Components/login/login.component.ts
@@ -24,7 +24,7 @@ export class LoginComponent {
 
   }
 
-  async loggin() {
+  async login() {
     this.usuario = (<HTMLInputElement>document.getElementById("usuario")).value;
     this.password = (<HTMLInputElement>document.getElementById("password")).value;
     console.log(this.auth.postLogin(this.usuario, this.password));
@@ -32,34 +32,6 @@ export class LoginComponent {
 
   
 
-  postLogin(username : String, password : String) {
-    let parser = new DOMParser();
-    let xmlString = '<?xml version="1.0" encoding="utf-8"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:sap-com:document:sap:rfc:functions">  <soapenv:Header/>   <soapenv:Body><urn:ZISUWM_WEB_LOGIN><PASSWORD>Israel123</PASSWORD><USUARIO>ADMIN</USUARIO></urn:ZISUWM_WEB_LOGIN></soapenv:Body> </soapenv:Envelope>';
-    
-    let doc = parser.parseFromString(xmlString, "application/xml");
-
-    console.log(doc);
-
-    let headers = new HttpHeaders()
-      .set('Content-Type', 'text/xml')
-      .set('charset', 'utf-8');
-
-    return new Promise(resolve => {
-      this.http.post("http://p15isudessap1.cisnergia.gob.ec:8000/sap/bc/srt/rfc/sap/zws_web_login/110/zws_web_login/zws_web_login", doc, {headers: headers}).subscribe(data => {
-        resolve(data);
-        console.log(data);
-        console.log(this.xmlStringToJson(data.toString()));
-        alert("LLEGO A ESTE METODO -------->"+ data)
-      }, err => {
-        console.log(err);
-        alert("ERROR ---->"+ err.value)
-      });
-    });
-  }
-  xmlStringToJson(arg0: string): any {
-    throw new Error('Method not implemented.');
-  }
-  
 
 
 }
