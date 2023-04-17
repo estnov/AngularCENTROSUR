e58cb90f29cc08f970053d1a501a82e67aa8e1ae
ronald175 <ronaldandrade456@gmail.com>
2023-04-17 14:33:17 -0500

    DATE

--- proyectoCentroSur/src/app/Components/modificar/modificar.component.ts
+++ proyectoCentroSur/src/app/Components/modificar/modificar.component.ts
@@ -7,6 +7,11 @@ import { ModifyService } from 'src/app/services/modify.service';
   styleUrls: ['./modificar.component.scss']
 })
 export class ModificarComponent {
+
+  public dateStart_selected:any;
+  public dateEnd_selected:any;
+  public date_current:any;
+
   orden = localStorage.getItem('orden');
 
   constructor(private modify: ModifyService) { }
@@ -17,5 +22,23 @@ export class ModificarComponent {
   async modificar(){
     //this.modify.mod();
   }
+  myFilter = (d: Date | null): boolean => {
+    const day = (d || new Date()).getDay();
+    // Prevent Saturday and Sunday from being selected.
+    return day !== 0 && day !== 6;
+  };
 
+  add_zero_date(data:any) {
+    return (data < 10) ? '0' + data.toString() : data;
+  }
+  
+  get_date_current() {
+    const hoy = new Date();
+    const dd = hoy.getDate();
+    const mm = hoy.getMonth() + 1;
+    const yyyy = hoy.getFullYear();
+    this.dateEnd_selected = yyyy + '-' + this.add_zero_date(mm) + '-' + this.add_zero_date(dd);
+    this.dateStart_selected = yyyy + '-' + this.add_zero_date(mm) + '-' + this.add_zero_date(dd);
+    this.date_current = yyyy + '-' + this.add_zero_date(mm) + '-' + this.add_zero_date(dd);
+  }
 }
--- proyectoCentroSur/src/app/app.module.ts
+++ proyectoCentroSur/src/app/app.module.ts
@@ -14,9 +14,11 @@ import {MatTableModule} from '@angular/material/table';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatMenuModule} from '@angular/material/menu';
 import { ModificarComponent } from './Components/modificar/modificar.component';
-
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
 
 
 @NgModule({
@@ -36,7 +38,10 @@ import {MatButtonModule} from '@angular/material/button';
     MatTableModule,
     MatMenuModule,
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
